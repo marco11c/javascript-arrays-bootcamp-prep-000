a54f5782e09179e20632a4bf2fe250f4afb8becd
cayetanomarco@yahoo.com <cayetanomarco@yahoo.com>
2018-07-22 03:43:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = [
+    'snickers',
+    'hundred grand',
+    'kitkat',
+    'skittles', //NOT EVEN CHOCOLATE!!!
+    
+  ]
+  
+  //use push to add new elements to an array
+  //unshift add elements at beginning of array
+  //spread operator (...) spreads arrays contents
+  
+  function addElementToBeginningOfArray(myarr,myel) {
+    
+  }
+  function destructivelyAddElementToBeginningOfArray(myarr,myel) {
+    
+  }  
+  
